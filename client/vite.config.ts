33f4d86cf42a9b77fede154fee0ff20e8742c25d
ArcobalenoX98 <arocobalenox@gmail.com>
2025-08-21 14:21:27 +0100

    vite.config chage

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) =>{
+  const env = loadEnv(mode, process.cwd())
+  console.log('Mode:',mode)
+  console.log('VITE_API_BASE:',env.VITE_API_BASE)
+
+  return{
   plugins: [vue()],
   resolve:{
     alias:{
@@ -24,4 +29,4 @@ export default defineConfig({
       }
     }
   }
-})
+}})
